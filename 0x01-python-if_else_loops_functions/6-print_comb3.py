312abea2751972af329a5ddc596706b9593b1d87
Deborah <semiloremuriana@gmail.com>
2022-06-28 14:14:32 -0700

    7 and 8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(10):
         if (i != j and i < j):
-            if (i != 8 and j != 9):
+            if (i != 8 or j != 9):
                 print("{0}{1}".format(i, j), end=", ")
             else:
                 print("{0}{1}".format(i, j))
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for a in str:
+        if (ord(a) >= 97 and ord(a) <= 122):
+            print("{0:c}".format(ord(a) - 32), end="")
+        else:
+            print("{0:c}".format(ord(a)), end="")
