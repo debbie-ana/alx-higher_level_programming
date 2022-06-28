6897823a43f86ebfd97cf799a15f7b549e78483e
Deborah <semiloremuriana@gmail.com>
2022-06-28 13:43:03 -0700

    5 and 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if (i != j and i < j):
+            if (i != 8 and j != 9):
+                print("{0}{1}".format(i, j), end=", ")
+            else:
+                print("{0}{1}".format(i, j))
