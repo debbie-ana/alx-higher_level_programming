2e10eeea53c2276b8e6d6466eebb05368dd88367
Deborah <semiloremuriana@gmail.com>
2022-06-30 06:00:53 -0700

    0.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ from add_0 import add
 if __name__ != "0-add":
     a = 1
     b = 2
-    c = add(a, b)
-    print("{0} + {1} = {2}".format(a, b, c))
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
