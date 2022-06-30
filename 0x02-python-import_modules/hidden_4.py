1d130a24feb01bb16e2c0cd56f633a542ec065a5
Deborah <semiloremuriana@gmail.com>
2022-06-30 09:59:58 -0700

    4

---
+++ 0x02-python-import_modules/hidden_4.py
@@ -0,0 +1,23 @@
+# uncompyle6 version 3.8.0
+# Python bytecode 3.8.0 (3413)
+# Decompiled from: Python 3.8.10 (default, Nov 26 2021, 20:14:08) 
+# [GCC 9.3.0]
+# Embedded file name: hidden_4.py
+# Compiled at: 2021-07-16 10:16:33
+# Size of source mod 2**32: 146 bytes
+
+
+def __init__():
+    return True
+
+
+def my_secret_santa():
+    return True
+
+
+def print_school():
+    return True
+
+
+def print_hidden():
+    return True
