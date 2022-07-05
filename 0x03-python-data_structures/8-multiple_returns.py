e5c1e73d177c63db0de5e30828727a51304d4b3b
Deborah <semiloremuriana@gmail.com>
2022-07-04 18:33:33 -0700

    8 and 9

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
+    length = len(sentence)
     if sentence:
-        i = sentence[0]
+        first = sentence[0]
     else:
-        i = None
-    return (l, i)
+        first = None
+    return (length, first)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list <= 0):
+    if len(my_list) <= 0:
         return None
     a = my_list[0]
     for num in my_list:
