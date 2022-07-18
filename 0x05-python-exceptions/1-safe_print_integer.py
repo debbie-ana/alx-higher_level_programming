4e90ab51ec47bf23b32464e881b686376ddd3433
Deborah <semiloremuriana@gmail.com>
2022-07-18 08:49:06 -0700

    again

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        printf("{:d}".format(value))
+        print("{:d}".format(value))
         return True
     except ValueError:
         return False
