2edc794b75878bf0f30902e3fae9925988117dd5
Deborah <semiloremuriana@gmail.com>
2022-07-19 16:34:43 -0700

    5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -45,3 +45,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def my_print(self):
+        """ prints square"""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
