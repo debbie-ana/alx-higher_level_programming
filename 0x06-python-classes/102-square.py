c6807f33d316bc44d5732a3e32274cbb47fefce7
Deborah <semiloremuriana@gmail.com>
2022-07-19 18:38:44 -0700

    8 and 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """Defines a class Square
+    Attributes:
+        __size (int): length of a side of square
+    """
+
+    def __init__(self, size=0):
+        """Initialisation of class
+
+        Args:
+            size (int): length of square
+        """
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square
+        Returns:
+            Area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """getter method for size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter method for size
+        Args:
+            value (int): value for size to be set
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def __eq__(self, other):
+        """define == comparison operator"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """define != comparison operator"""
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        """define < comparison operator"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """define <= comparison operator"""
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        """define > comparison operator"""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """define >= comparison operator"""
+        return self.area() >= other.area()
