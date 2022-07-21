7cf4504c81ac840268e5dda59bcda3fe6cb6f489
Deborah <semiloremuriana@gmail.com>
2022-07-20 18:11:48 -0700

    8 again

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -97,7 +97,7 @@ class Square:
             [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
                 print("")
-            return ("")
+        return ("")
