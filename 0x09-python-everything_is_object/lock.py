301abf8b32b7a291defbced0a7ae8c5cebbc7aa4
Deborah <semiloremuriana@gmail.com>
2022-07-26 17:47:00 -0700

    try

--- 0x09-python-everything_is_object/lock.py
+++ 0x09-python-everything_is_object/lock.py
@@ -2,5 +2,6 @@
 class LockedClass:
     def __setattr__(self, attr, value):
         if attr != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
+            raise AttributeError("'LockedClass' object has no attribute '{}'"
+                                 .format(attr))
         self.__dict__.update({attr: value})
