09940da7322f5d3881c433a83782417ca3e36c69
Deborah <semiloremuriana@gmail.com>
2022-08-07 11:51:31 -0700

    3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,4 +8,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("my name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""print square module"""
+
+def print_square(size):
+    """this function prints a square
+    given the size length of the square"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for a in range(size):
+        print("".join(["#" for b in range(size)]))
