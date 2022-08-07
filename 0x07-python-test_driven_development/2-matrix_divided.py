d91dbbb879034056f3eca5d365c275aa5d06ab33
Deborah <semiloremuriana@gmail.com>
2022-08-07 09:18:19 -0700

    1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,18 +6,21 @@ def matrix_divided(matrix, div):
     """returns a new matrix after dividing
     each elements in the matrix"""
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError
+    ("matrix must be a matrix (list of lists) of integers/floats")
     size = None
     for row in matrix:
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError
+        ("matrix must be a matrix (list of lists) of integers/floats")
         if size is None:
             size = len(row)
         elif size != len(row):
             raise TypeError("Each row of the matrix must have the same size")
         for a in row:
             if type(a) is not int and type(a) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError
+            ("matrix must be a matrix (list of lists) of integers/floats")
 
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
--- 0x07-python-test_driven_development/some.py
+++ 0x07-python-test_driven_development/some.py
@@ -17,9 +17,89 @@ Checking for function docstring:
     >>> len(fd) > 1
     True
 
-Regular test:
+Test(matrix = list of lists of integers and div = integer):
     >>> matrix = [[4, 6, 8], [2, 9, 3]]
     >>> print(matrix_divided(matrix, 2))
     [[2.0, 3.0, 4.0], [1.0, 4.5, 1.5]]
     >>> print(matrix)
     [[4, 6, 8], [2, 9, 3]]
+
+Test(matrix = list of lists of integers and div = string):
+    >>> matrix = [[1, 4, 6], [4, 1, 0]]
+    >>> print(matrix_divided(matrix, "free"))
+    Traceback (most recent call last):
+    ...
+    TypeError: div must be a number
+
+Test(div = 0):
+    >>> matrix = [[1, 9], [6, 2]]
+    >>> print(matrix_divided(matrix, 0))
+    Traceback (most recent call last):
+    ...
+    ZeroDivisionError: division by zero
+
+Test(matrix is not list of lists of integers and div = integer):
+    >>> matrix = (1, 4, 5)
+    >>> print(matrix_divided(matrix, 2)
+    Traceback (most recent call last):
+    ...
+    TypeError: matrix must be a matrix (list of lists) of integers/floats
+
+Test(matrix = list of lists of integers but not rectangular and div = integer):
+    >>> matrix = [[1, 2], [5, 9, 0, 1]]
+    >>> print(matrix_divided(matrix, 3))
+    Traceback (most recent call last):
+    ...
+    TypeError: Each row of the matrix must have the same size
+
+Test(div = float(inf)):
+    >>> matrix = [[1, 2], [2, 4]]
+    >>> print(matrix_divided(matrix, float('inf')))
+    [[0.0, 0.0], [0.0, 0.0]]
+
+Test(matrix has one float(inf):
+    >>> matrix = [[5, float('inf')], [0, 1]]
+    >>> print(matrix_divided(matrix, 2))
+    [[2.5, inf], [0, 0.5]]
+
+Test(matrix has one float(nan):
+    >>> matrix = [[2, 3], [2, float('nan')]]
+    >>> print(matrix_divided(matrix, 2))
+    [[1, 1.5], [1, nan]]
+
+Test(matrix = list of lists of integers with one float(inf)
+    and div = float(inf):
+    >>> matrix = [[3, 6, 5][float('inf'), 1, 0]]
+    >>> print(matrix_divided(matrix, float('inf'))
+    [[0.0, 0.0, 0.0], [nan, 0.0, 0.0]]
+
+Test(matrix = list of lists of integers with one float(inf)
+    and div = float(nan):
+    >>> matrix = [[3, 6, 5][float('inf'), 1, 0]]
+    >>> print(matrix_divided(matrix, float('nan'))
+    [[nan, nan, nan], [nan, nan, nan]]
+
+Test(matrix = list of lists of integers with one float(nan)
+    and div = float(inf):
+    >>> matrix = [[3, 6, 5][float('nan'), 1, 0]]
+    >>> print(matrix_divided(matrix, float('inf'))
+    [[0.0, 0.0, 0.0], [nan, 0.0, 0.0]]
+
+Test(matrix = list of lists of integers with one float(nan)
+    and div = float(nan):
+    >>> matrix = [[3, 6, 5][float('nan'), 1, 0]]
+    >>> print(matrix_divided(matrix, float('nan'))
+    [[nan, nan, nan], [nan, nan, nan]]
+
+Test(missing one argument):
+    >>> matrix = [[1, 2][2, 5]]
+    >>> print(matrix_divided(matrix))
+    Traceback (most recent call last):
+    ...
+    TypeError: matrix_divided() missing 1 required positional argument: 'div'
+
+Test(missing 2 arguments):
+    >>> print(matrix_divided())
+    Traceback (most recent call last):
+    ...
+    TypeError: matrix_divided() missing 2 required positional arguments: 'matrix' and 'div'
