e8b77d3192436ed4ce90041001a7a9b61d3e654f
debbie-ana <semiloremuriana@gmail.com>
2023-03-10 10:30:04 -0800

    correction again

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """convert to json string"""
         if list_dictionaries == None:
             return "[]"
         else:
