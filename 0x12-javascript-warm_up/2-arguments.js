093a7210eb1911af31704a2e72c50e798d96e008
debbie-ana <semiloremuriana@gmail.com>
2023-04-12 01:15:37 -0700

    task 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const process = require('process');
-let args = process.argv;
-if (args.length < 3)
-{
-	console.log('No argument');
-} else if (args.length < 4) {
-	console.log('Argument found');
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length < 4) {
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
