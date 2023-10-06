59e743703b782eec01967c72912930da0f77bbc7
debbie-ana <semiloremuriana@gmail.com>
2023-10-06 15:19:59 -0700

    test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module for integer addition
+contains function add_integer
+"""
+
+
+def add_integer(a, b=98):
+    """returns the addition of 2 integers"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""text indentation module"""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after the characters
+    ., ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for word in text:
+        if flag == 0:
+            if word == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if word == '?' or word == '.' or word == ':':
+                print(word)
+                print()
+                flag = 0
+            else:
+                print(word, end="")
